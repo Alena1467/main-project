9bc178e55767a67b80d4c271164b0d162e29cddd
Alena1467 <mirgalena19@gmail.com>
2023-04-24 15:16:04 +0500

    details str

--- api/core.php
+++ api/core.php
@@ -19,4 +19,10 @@ abstract class Core {
     public function get_body() {
         include "template/index.php";
     }
+    public function formatstr($str){
+        $str = trim($str);
+        $str = stripslashes($str);
+        $str = htmlspecialchars($str);
+        return $str;
+    }
 }
--- api/modules/mod_list.php
+++ api/modules/mod_list.php
@@ -1,14 +1,34 @@
+<form method="POST" action="<?php echo $_SERVER["PHP_SELF"] ?>">
+  <label for="age"></label>
+  <input type="range" id="age" name="age" min="0" value="123" max="123" step="1" oninput="level.value = age.valueAsNumber" >
+  <output for="age" name="level">123</output>
+  <select name="sort">
+    <option value="asc">по возрастанию</option>
+    <option value="desc">по убыванию</option>
+  </select>
+  <input type="submit" value="фильтровать">
+</form>
+
+
 <?php
 
-//код запроса
-$sql = "SELECT * FROM `students`";
+$extra_sql = " ";
+if(isset($_POST['age'])){
+    $age = $this->formatstr($_POST['age']);
+    $extra_sql .= " WHERE age < $age";
+  }
+ 
+  if(isset($_POST['sort'])){
+    $sort = $this->formatstr($_POST['sort']);
+    $extra_sql .= " ORDER BY age $sort";
+  }
+ 
+// request run
+$result=$this->connect->query("SELECT * FROM students".$extra_sql);
+// output request result
 
-//выполнить запрос
-$result = $this->connect->query($sql);
-
-//вывести результаты запроса на страницу
-while ($row = $result->fetch_assoc()){
-    echo "<div>
-            $row[lname], $row[fname], $row[gender], $row[age]
-        </div>";
+while($row = $result->fetch_assoc()){
+    echo "<div><a href='?option=details&id=$row[student_id]'>$row[lname], $row[fname], $row[gender], $row[age]</a></div>";
 }
+
+?>
