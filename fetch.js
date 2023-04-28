ae185baf9e54935150d0e4a04d01e4656f9f522c
Alena1467 <mirgalena19@gmail.com>
2023-04-28 16:18:30 +0500

    async await

--- fetch.js
+++ fetch.js
@@ -3,31 +3,26 @@ const output = document.querySelector(".profile");
 
 formAuth.addEventListener("submit", auth);
 
-function auth(event){
+async function auth(event){
     event.preventDefault();//отменяем отправку формы
     let data = new FormData(formAuth);//собираем данные с формы
-    fetch("api/auth.php", {
+    
+    const response = await fetch("api/auth.php", {
         method: 'POST',
         'Content-Type':'application/json',
         body: data
+    });
+    
+json = await response.json();
+console.log(json);
+    if(json.status){
+        output.innerHTML = "вы авторизованы как " + json.name;
+        formAuth.style.display = "none";
     }
-    ).then(
-        (response)=>{
-            return response.json();
+    else {
+        let p  = document.createElement("p");
+        p.innerHTML = "ошибка авторизации";
+        output.prepend(p);
     }
-    ).then(
-        (json) => { 
-            console.log(json);
-            if(json.status){
-                output.innerHTML = "вы авторизованы как " + json.name;
-                formAuth.style.display = "none";
-            }
-            else {
-                let p  = document.createElement("p");
-                p.innerHTML = "ошибка авторизации";
-                output.prepend(p);
-            }
-        }
-    )
 }
 
